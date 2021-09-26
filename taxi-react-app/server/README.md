272fac0b286e477401e9fb632f118042e4a3186d
Xiang Chen <xiangchenchen96@gmail.com>
2021-09-26 10:28:34 +0200

    :wrench: Getting started

---
+++ taxi-react-app/server/README.md
@@ -0,0 +1,13 @@
+# taxi-react-app
+https://testdriven.io/courses/taxi-react
+
+```
+$env:PGDATABASE="taxi"
+$env:PGUSER="taxi"
+$env:PGPASSWORD="taxi"
+```
+
+```
+python manage.py runserver
+python manage.py createsuperuser
+```
--- taxi-react-app/server/taxi/asgi.py
+++ taxi-react-app/server/taxi/asgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import django
 
-from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxi.settings')
-
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taxi.settings")
+django.setup()
+application = get_default_application()
